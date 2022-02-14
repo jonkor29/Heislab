c28f682ff4ebef45feb425df84b0254607d2fdf8
jonkor-elementlogic <jonas.korkosh@elementlogic.no>
2022-02-14 16:42:04 +0100

    added comments

--- skeleton_project/source/driver/elevio.h
+++ skeleton_project/source/driver/elevio.h
@@ -18,16 +18,24 @@ typedef enum {
     BUTTON_CAB          = 2
 } ButtonType;
 
-
+//INIT
 void elevio_init(void);
 
+//MOTOR
+//This function runs the elevator up/down/stops it based on MotorDirection
 void elevio_motorDirection(MotorDirection dirn);
+
+// - - - LAMPS - - -
+//Lights up lamp at a certain floor. We assume value is 0/1 and ButtonType is wether the button is inside or outside the elevator
 void elevio_buttonLamp(int floor, ButtonType button, int value);
+//TEST this func! Probably lights up floor indicator lamp
 void elevio_floorIndicator(int floor);
 void elevio_doorOpenLamp(int value);
 void elevio_stopLamp(int value);
 
+//Unclear how this works... How will we get input from buttons? Polling?
 int elevio_callButton(int floor, ButtonType button);
+//
 int elevio_floorSensor(void);
 int elevio_stopButton(void);
 int elevio_obstruction(void);
