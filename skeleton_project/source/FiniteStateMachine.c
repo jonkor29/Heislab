a3b7b84fb8fb639bb4936a180c8baf0198d59f87
Jonas Korkosh <jonaskorkosh@gmail.com>
2022-02-25 17:08:27 +0100

    Added State and Init()

--- skeleton_project/source/FiniteStateMachine.c
+++ skeleton_project/source/FiniteStateMachine.c
@@ -1,1 +1,12 @@
-#include "FiniteStateMachine.h" 
+#include "FiniteStateMachine.h"
+
+#include "driver/elevio.h"
+#include "Order.h"
+#include "LinkedList.h"
+
+void init() {
+    elevio_motorDirection(DIRN_DOWN);
+    while(elevio_floorSensor() == -1) { 
+
+    }
+}
--- skeleton_project/source/FiniteStateMachine.h
+++ skeleton_project/source/FiniteStateMachine.h
@@ -1,9 +1,16 @@
 #pragma once
 
-#include "driver/elevio.h"
-#include "Order.h"
-#include "LinkedList.h"
+
 
 //tanke: kan lage en buttonAndSensorPoller klasse som poller alt og returnerer en struct InputFSM som input til FSM
 //Denne kan ha medlemsvariabler for alle de nødvendige knappene
-//denne klassen tar inn input fra polleren og setter alle outputs. 
+//denne klassen tar inn input fra polleren og setter alle outputs.
+
+typedef enum {
+    IDLE = 0,
+    MOVING_DOWN = 1,
+    MOVING_UP = 2,
+    DOORS_OPEN = 3
+} State;
+
+void init();
