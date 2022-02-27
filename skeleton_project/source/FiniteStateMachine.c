57857b1ee17c4694f754a3a521b683c0b15a3a6b
Jonas Korkosh <jonaskorkosh@gmail.com>
2022-02-27 19:52:35 +0100

    Added logic for IDLE and MOVING_DOWN

--- skeleton_project/source/FiniteStateMachine.c
+++ skeleton_project/source/FiniteStateMachine.c
@@ -61,27 +61,80 @@ if (!contains_order(p_head, current_order))
 
 //main loop
 void run_elevator() {
-    init_FSM();
-    State current_state = IDLE;
+    State state = IDLE;
     Node* head = NULL;
     Order current_order;
+    Floor current_floor;
     //update_current_order
 
-    while (1) {
-        switch (current_state)
+    init_FSM();
+
+    while (state != MOVING_UP) {  
+        //kanskje denne if-statementen kan flyttes til inni hver state for raskere oppdatering?
+        if (elevio_floorSensor() != -1) 
+        {
+            current_floor = (Floor)(elevio_floorSensor());
+        }
+          
+        switch (state) 
         {
         case IDLE:
-            //do this in state
-            //if condition, update state
+            //elevio_motorDirection(DIRN_STOP);
+            while (state == IDLE)
+            {
+                look_for_and_add_order(&head);
+                if (head != NULL) 
+                {
+                    //update_current_order(&head, &current_order);
+                    current_order = (*head).order;
+                    if (current_order.floor < current_floor){
+                        state = MOVING_DOWN;
+                    } 
+                    else if(current_order.floor > current_floor){
+                        state = MOVING_UP;
+                    }
+                    else if(current_order.floor == current_floor){
+                        state = DOORS_OPEN;
+                    }
+                }               
+            }
             break;
         
         case MOVING_DOWN:
+            elevio_motorDirection(DIRN_DOWN);
+ 
+            while (state == MOVING_DOWN) 
+            {   
+                look_for_and_add_order(&head);
+                int floor_sensor_reading = elevio_floorSensor();
+                if (floor_sensor_reading != -1) 
+                {    
+                    elevio_floorIndicator(floor_sensor_reading);
+  
+                    Order eligible_order1 = {DOWN, (Floor)floor_sensor_reading};
+                    Order eligible_order2 = {CAB, (Floor)floor_sensor_reading}; 
+                    
+                    if (current_order.floor == floor_sensor_reading || contains_order(&head, eligible_order1) || contains_order(&head, eligible_order2)) 
+                    {
+                        state = DOORS_OPEN;
+                            
+                    } 
+                } 
+            } 
+            
+            
+
             break;
         
         case MOVING_UP:
+            elevio_motorDirection(DIRN_UP);
+
+            
             break;
 
         case DOORS_OPEN:
+            elevio_motorDirection(DIRN_STOP);
+            elevio_doorOpenLamp(1);            
             break;
         
         
@@ -89,4 +142,5 @@ void run_elevator() {
             break;
         }
     }
+    delete_all(&head);
 }
