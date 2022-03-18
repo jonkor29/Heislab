ef6c95794885bd8d38b79f5f720da7d187b06824
jonkor-elementlogic <jonas.korkosh@elementlogic.no>
2022-03-18 10:24:11 +0100

    Updated prefixes in LinkedList

--- skeleton_project/source/LinkedList.c
+++ skeleton_project/source/LinkedList.c
@@ -5,93 +5,93 @@
 
 #include "LinkedList.h"
 
-//Dropper p-prefiks foran ord som head, last og next fordi det er godt kjent for programmerere at disse er pekere. Dette for å unngå forvirring.
-void append(Node** p_head, Order new_order) {
+//Dropper p-prefiks foran ord som head, p_last og next fordi det er godt kjent for programmerere at disse er pekere. Dette for å unngå forvirring.
+void append(Node** pp_head, Order new_order) {
     Node* p_new_node = (Node*) malloc(sizeof(Node)); //allocate new node
-    Node* last = *p_head; //set last equal to head
+    Node* p_last = *pp_head; //set p_last equal to head
     
     p_new_node->order = new_order; //load the new order into the new node
-    p_new_node->next = NULL; //the new node wil be the last node (we're appending)
+    p_new_node->p_next = NULL; //the new node wil be the p_last node (we're appending)
 
-    if (*p_head == NULL) {
-        *p_head = p_new_node; //if the list was empty, set head to point to teh new node
+    if (*pp_head == NULL) {
+        *pp_head = p_new_node; //if the list was empty, set head to point to teh new node
         return;
     }
 
-    while(last->next != NULL) { //find the last node, and set last 
-        last = last->next;
+    while(p_last->p_next != NULL) { //find the p_last node, and set p_last 
+        p_last = p_last->p_next;
     }
 
-    last->next = p_new_node; //make the node that was last now point to the new node
+    p_last->p_next = p_new_node; //make the node that was p_last now point to the new node
 
     return;
 }
-
-void delete_all_orders(Node** p_head) {
-    Node* head = *p_head;
-    while(head != NULL) {
-        Node* temp = head;
-        head = head->next;
-        free(temp);
-        temp = NULL;
+//temp, head, p_prev
+void delete_all_orders(Node** pp_head) {
+    Node* p_head = *pp_head;
+    while(p_head != NULL) {
+        Node* p_temp = p_head;
+        p_head = p_head->p_next;
+        free(p_temp);
+        p_temp = NULL;
     }
-    *p_head = NULL; // setter den faktiske head-pointeren lik NULL
+    *pp_head = NULL; // setter den faktiske p_head-pointeren lik NULL
 }
 
-void delete_orders(Node** p_head, Floor current_floor) {
-    Node* temp = *p_head;
-    Node* prev;
+void delete_orders(Node** pp_head, Floor current_floor) {
+    Node* p_temp = *pp_head;
+    Node* p_prev;
     
-    while(temp != NULL && temp->order.floor == current_floor) {
-        *p_head= temp->next;
-        free(temp);
-        temp = *p_head;
+    while(p_temp != NULL && p_temp->order.floor == current_floor) {
+        *pp_head= p_temp->p_next;
+        free(p_temp);
+        p_temp = *pp_head;
     }
 
-    while (temp != NULL) {   
-        while(temp != NULL && temp->order.floor!=current_floor) {
-            prev=temp;
-            temp=temp->next;
+    while (p_temp != NULL) {   
+        while(p_temp != NULL && p_temp->order.floor!=current_floor) {
+            p_prev=p_temp;
+            p_temp=p_temp->p_next;
         }
 
-        if(temp == NULL) {
+        if(p_temp == NULL) {
             return;
         }
 
-        prev->next=temp->next;
-        free(temp);
-        temp= prev->next;    
+        p_prev->p_next=p_temp->p_next;
+        free(p_temp);
+        p_temp= p_prev->p_next;    
     }
 
     return;
 }
 
-int contains_order(Node** p_head, Order current_order) {
+int contains_order(Node** pp_head, Order current_order) {
     
-    Node* temp = *p_head;
-    Node* prev;
+    Node* p_temp = *pp_head;
+    Node* p_prev;
     
-    while(temp != NULL && (temp->order.floor!=current_order.floor || temp->order.type != current_order.type)) {
-        prev=temp;
-        temp=temp->next;
+    while(p_temp != NULL && (p_temp->order.floor!=current_order.floor || p_temp->order.type != current_order.type)) {
+        p_prev=p_temp;
+        p_temp=p_temp->p_next;
     }
 
-    if(temp == NULL) {
+    if(p_temp == NULL) {
         return 0;
     }
 
-    if (temp->order.floor==current_order.floor && temp->order.type == current_order.type) {
+    if (p_temp->order.floor==current_order.floor && p_temp->order.type == current_order.type) {
         return 1;
     }  
 
     return 0;
 }
 
-void print_list(Node* node) {
+void print_list(Node* p_node) {
     printf("[");
-    while(node != NULL) {
-        printf(" (type: %d, floor: %d) ,", node->order.type, node->order.floor);
-        node = node->next;
+    while(p_node != NULL) {
+        printf(" (type: %d, floor: %d) ,", p_node->order.type, p_node->order.floor);
+        p_node = p_node->p_next;
     }
     printf("]");
 }
--- skeleton_project/source/LinkedList.h
+++ skeleton_project/source/LinkedList.h
@@ -1,10 +1,6 @@
 /**
  * @file LinkedList.h
- * @author Arvin.D and Jonas.K (jonask/arvind@stud.ntnu.no)
- * @brief A simple library for doing operations on a linkedlist containing  rders .
- * @date 2022-03-17
- * 
- * @copyright Copyright (c) 2022
+ * @brief A simple library for doing operations on a linkedlist containing Orders.
  * 
  */
 #pragma once
@@ -14,42 +10,42 @@
 
 typedef struct Node {
     Order order;
-    struct Node* next;
+    struct Node* p_next;
 } Node; 
 
 /**
- * @brief Append a @p new_order to the linkedlist which head pointer is @p p_head.
+ * @brief Append a @p new_order to the linkedlist which head pointer is @p pp_head.
  * 
- * @param[in, out] p_head   Head pointer to the linkedlist.
+ * @param[in, out] pp_head   Head pointer to the linkedlist.
  * @param[in] new_order     The order we are adding to the linkedlist.
  */
-void append(Node** p_head, Order new_order);
+void append(Node** pp_head, Order new_order);
 
 /**
- * @brief Deletes all orders in the linkedlist where @p p_head is pointing to the head
+ * @brief Deletes all orders in the linkedlist where @p pp_head is pointing to the head
  * 
- * @param[in,out] p_head    Head pointer to the linkedlist.
+ * @param[in,out] pp_head    Head pointer to the linkedlist.
  */
-void delete_all_orders(Node** p_head);
+void delete_all_orders(Node** pp_head);
 
 /**
- * @brief Delets all orders where the member variable floor is equal to @p current_floor in the linkedlist where @p p_head is the head pointer.
- * Delets all the orders containing @p current_floor in the linkedlist where @p p_head is the head pointer.  
+ * @brief Delets all orders where the member variable floor is equal to @p current_floor in the linkedlist where @p pp_head is the head pointer.
+ * Delets all the orders containing @p current_floor in the linkedlist where @p pp_head is the head pointer.  
 
- * @param[in, out] p_head   Head pointer to the linkedlist.
+ * @param[in, out] pp_head   Head pointer to the linkedlist.
  * @param[in] current_floor Current floor from floor_sensor(), delets the ordering containing this floor.
  */
-void delete_orders(Node** p_head, Floor current_floor);
+void delete_orders(Node** pp_head, Floor current_floor);
 
 /**
- * @brief Checks if @p current_order is in the linkedlist, or not. Where @p p_head is the head pointer.
+ * @brief Checks if @p current_order is in the linkedlist, or not. Where @p pp_head is the head pointer.
  * 
- * @param[in] p_head        Head pointer to the linkedlist.
+ * @param[in] pp_head        Head pointer to the linkedlist.
  * @param[in] current_order Checking if this orer is in the linkedlist or not.
  * 
  * @return @c 1 if @p current_order is in the linkedlist, or @c 0 otherwise. 
  */
-int contains_order(Node** p_head, Order current_order);
+int contains_order(Node** pp_head, Order current_order);
 
 /**
  * @brief Prints out all the orders in the linkedlist where @p node is the head. Used for debugging. 
